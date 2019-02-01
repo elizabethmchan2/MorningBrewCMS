33f3b072ccffd1ca5ed0460e7c53336e88518a09
elizabethmchan2 <elizabethmchan2@gmail.com>
2019-02-01 06:00:56 -0500

    fixed tinymce and added premium features

--- app/assets/javascripts/tinymce.js
+++ app/assets/javascripts/tinymce.js
@@ -2,11 +2,11 @@ $(document).on('ready page:load', function () {
     if (typeof tinyMCE != 'undefined') {
         tinyMCE.init({
             selector: "textarea.tinymce",
-            toolbar: [
-                "styleselect | bold italic strikethrough | alignleft aligncenter alignright alignjustify",
-                "bullist numlist outdent indent | link image | code | codesample"
-            ],
-            plugins: "image,link,code,codesample,autoresize,imagetools,media,table,insertdatetime,charmap,print,preview,anchor,searchreplace,visualblocks,fullscreen"
+            toolbar: "undo redo | styleselect | bold italic strikethrough underline forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview code" ,
+            plugins: "lists,textcolor, preview,image,link,code,codesample,autoresize,imagetools,media,table,insertdatetime,charmap,print,preview,anchor,searchreplace,visualblocks,fullscreen, wordcount, powerpaste",
+            powerpaste_allow_local_images: true,
+            powerpaste_word_import: 'prompt',
+            powerpaste_html_import: 'prompt',
         });
     } else {
         setTimeout(arguments.callee, 50);
